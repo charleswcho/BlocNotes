26321c175dc37cd799c52f13499a8e2a856fcabb
Charles Wesley Cho <charleswcho@gmail.com>
2015-07-22 19:51:42 -0700

    Create project

---
+++ BlocNotes/BlocNotes/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  BlocNotes
+//
+//  Created by Charles Wesley Cho on 7/22/15.
+//  Copyright (c) 2015 Charles Wesley Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
